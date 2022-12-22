0ed8b05d8dd399c71c1f50a1a8c7d7ae47b2bcaf
Elton Guilherme <elton.guilherme@hotmail.com>
2022-12-22 00:46:27 -0300

    Aula de declaração de tipos de variáveis

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
 ﻿using SintaxeETiposdeDadosC_.Models;
 
+string apresentacao = "Olá seja bem vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+
+
+/*
 Pessoa p = new Pessoa();
 
 p.Nome = "Buta";
@@ -7,4 +23,5 @@ p.Idade = 26;
 p.Apresentar();
 
 Pessoa pessoaFisicaRepresentacao = new Pessoa();
+*/
 
